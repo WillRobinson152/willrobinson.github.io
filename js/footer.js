2c3d2edf9e2512fc6c6fc1596b43738044d0892e
WillRobinson152 <robinsonwilliamarthur@gmail.com>
2024-12-27 15:24:05 -0600

    Fix footer.

--- js/footer.js
+++ js/footer.js
@@ -2,4 +2,4 @@ var today = new Date();
 var year = today.getFullYear();
 
 var footer = document.getElementById("copyright");
-footer.textContent = `<p>© ${year} Will Robinson</p>`
+footer.innerHTML = `<p>© ${year} Will Robinson</p>`
